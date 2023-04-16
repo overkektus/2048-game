4ce1b16e5f99d1998a396c420a475d40b5a136c4
Yahor Piskunou <egor.piskunou@gmail.com>
2023-04-16 19:21:02 +0200

    add keyboard events handlers

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -4,17 +4,55 @@ import Cell from "./Cell";
 import Tile from "./Tile";
 
 import { Grid } from "../core/grid";
+import { useEffect, useState } from "react";
 
 function GameBoard() {
   const grid = new Grid();
+
+  const [cells, setCells] = useState(grid.cells);
+
+  useEffect(() => {
+    const handleKeydown = (event: KeyboardEvent) => {
+      switch (event.key) {
+        case "ArrowUp":
+          grid.moveUp();
+          break;
+        case "ArrowDown":
+          console.log("down");
+          break;
+        case "ArrowLeft":
+          console.log("left");
+          break;
+        case "ArrowRight":
+          console.log("right");
+          break;
+        default:
+          break;
+      }
+      setCells([...grid.cells]);
+    };
+    window.addEventListener("keydown", handleKeydown);
+    return () => {
+      window.removeEventListener("keydown", handleKeydown);
+    };
+  }, []);
+
   return (
     <Board>
-      {grid.cells.map((cell) => (
+      {cells.map((cell) => (
         <Cell key={`${cell.x}-${cell.y}`} />
       ))}
-      {grid.tiles.map((tile) => (
-        <Tile rowPosition={tile.x} columnPosition={tile.y} value={tile.value} />
-      ))}
+      {cells.map(({ linkedTile }) => {
+        if (linkedTile)
+          return (
+            <Tile
+              key={`${linkedTile.x}-${linkedTile.y}`}
+              rowPosition={linkedTile.y}
+              columnPosition={linkedTile.x}
+              value={linkedTile.value}
+            />
+          );
+      })}
     </Board>
   );
 }
